559ff06b2f1824a45c7954d2d83eaa5fe7ceb5a0
Ndcas <ndhung9911@gmail.com>
2025-11-14 09:45:05 +0700

    sua conflict

--- Comic-Web-main/Frontend/src/App.jsx
+++ Comic-Web-main/Frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
 import "./App.css";
 
 function App() {
@@ -39,22 +38,6 @@ function App() {
           <Link to="/test" className="btn">🧪 Trang Test</Link>
         </div>
       </section>
-=======
-import './App.css';
-
-function App() {
-  return (
-    <div className="p-6 space-y-4">
-      <h1 className="text-2xl font-bold">Trang chính</h1>
-
-      <div className="flex flex-col gap-3 w-64">
-        <Link to="/login" className="btn">Đăng nhập</Link>
-        <Link to="/register" className="btn">Đăng ký</Link>
-        <Link to="/forgot-password" className="btn">Quên mật khẩu</Link>
-        <Link to="/change-password" className="btn">Đổi mật khẩu</Link>
-        <Link to="/test" className="btn">Đi đến Test</Link>
-      </div>
->>>>>>> 8e71f91dafaadcd63f256a91d20e5fe2be7f15bf
     </div>
   );
 }
